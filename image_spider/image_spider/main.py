834af25dfc1d12654993a844a6d65e5ce34c7618
caiyueliang <393900414@qq.com>
2019-04-03 10:39:40 +0800

    update

--- image_spider/image_spider/main.py
+++ image_spider/image_spider/main.py
@@ -1,7 +1,10 @@
 from scrapy import cmdline
-
+import subprocess
 
 if __name__ == '__main__':
+    # keyword_list = ['厨师帽', '安全帽', '厨师服', '口罩', '电风扇',
+    #                 '电视机', '电冰箱', '洗衣机', '空调', '微波炉',
+    #                 '炒锅', '砧板', '桌子', '椅子', '沙发']
     keyword_list = ['五星红旗', '中国共产党党旗', '八一军旗', '美国国旗', '英国国旗',
                     '法国国旗', '日本国旗', '朝鲜国旗', '韩国国旗', '俄罗斯国旗',
                     '西班牙国旗', '奥运会会旗', '联合国旗帜', '欧盟旗帜', '菲律宾共和国国旗',
@@ -11,5 +14,6 @@ if __name__ == '__main__':
     # cmdline.execute("scrapy crawl image -o result.csv".split())    # 用命令行启动
     for keyword in keyword_list:
         print(keyword)
-        cmdline.execute(("scrapy crawl image -a keyword="+keyword).split())  # 用命令行启动
+        # cmdline.execute(("scrapy crawl image -a keyword="+keyword).split())  # 用命令行启动
+        subprocess.call("scrapy crawl image -a keyword="+keyword, shell=True)
 
